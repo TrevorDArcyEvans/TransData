f8612a7d6f0ca588bf4c49336b428e864f63ecef
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2025-02-04 19:44:19 +0000

    - skeleton app

---
+++ src/TransData.App/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace TransData.App.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ src/TransData.App/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace TransData.App.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
