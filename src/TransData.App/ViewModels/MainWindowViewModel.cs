89651ce92f4e1abd698ad1823ebf9133f9b15fc6
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2025-02-04 22:04:46 +0000

    - added menus

--- src/TransData.App/ViewModels/MainWindowViewModel.cs
+++ src/TransData.App/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,26 @@
-﻿namespace TransData.App.ViewModels;
+﻿using System.Reactive;
+using Avalonia.Controls.ApplicationLifetimes;
+using ReactiveUI;
+
+namespace TransData.App.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to TransData - Transforming Your Data Across Platforms";
+  public ReactiveCommand<Unit, Unit> ExitCommand { get; }
+
+  private readonly App _parent;
+
+  public MainWindowViewModel(App parent)
+  {
+    _parent = parent;
+    ExitCommand = ReactiveCommand.Create(DoExitCommand);
+  }
+
+  private void DoExitCommand()
+  {
+    if (_parent.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime app)
+    {
+      app.Shutdown();
+    }
+  }
 }
