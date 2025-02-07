2048e2cd26eb5b4dede383219a5ed94b01e2abd9
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2025-02-07 21:53:13 +0000

    - renamed to remove ambiguity

--- src/TransData.App/Views/MainWindow.axaml.cs
+++ src/TransData.App/Views/MainWindow.axaml.cs
@@ -7,39 +7,38 @@ namespace TransData.App.Views;
 
 public partial class MainWindow : Window
 {
-    public MainWindow(MainWindowViewModel viewModel)
+  public MainWindow(MainWindowViewModel viewModel)
+  {
+    InitializeComponent();
+
+    DataContext = viewModel;
+
+    viewModel.PropertyChanged += OnPropertyChanged;
+  }
+
+  private void OnPropertyChanged(object? sender, PropertyChangedEventArgs e)
+  {
+    var vm = (MainWindowViewModel) DataContext!;
+    if (e.PropertyName == nameof(vm.InputDataTable))
     {
-        InitializeComponent();
-        
-        DataContext = viewModel;
-        
-        viewModel.PropertyChanged += OnPropertyChanged;
+      // clear out any existing columns
+      while (InputDataGrid.Columns.Count > 0)
+      {
+        InputDataGrid.Columns.RemoveAt(InputDataGrid.Columns.Count - 1);
+      }
+
+      // assign the datatable to the grid
+      InputDataGrid.ItemsSource = vm.InputDataTable.DefaultView;
+
+      // create the grid columns based on the datatables columns
+      foreach (System.Data.DataColumn x in vm.InputDataTable.Columns)
+      {
+        DataGridBoundColumn gridCol = x.DataType == typeof(bool) ? new DataGridCheckBoxColumn() : new DataGridTextColumn();
+
+        gridCol.Header = x.ColumnName;
+        gridCol.Binding = new Avalonia.Data.Binding($"Row.ItemArray[{x.Ordinal}]");
+        InputDataGrid.Columns.Add(gridCol);
+      }
     }
-    
-    private void OnPropertyChanged(object? sender, PropertyChangedEventArgs e)
-    {
-        Debug.WriteLine(e.PropertyName);
-        var vm = (MainWindowViewModel) DataContext!;
-        if (e.PropertyName == nameof(vm.InputDataTable))
-        {
-            // clear out any existing columns
-            while (InputDataTable.Columns.Count > 0)
-            {
-                InputDataTable.Columns.RemoveAt(InputDataTable.Columns.Count - 1);
-            }
-
-            // assign the datatable to the grid
-            InputDataTable.ItemsSource = vm.InputDataTable.DefaultView;
-
-            // create the grid columns based on the datatables columns
-            foreach (System.Data.DataColumn x in vm.InputDataTable.Columns)
-            {
-                DataGridBoundColumn gridCol = x.DataType == typeof(bool) ? new DataGridCheckBoxColumn() : new DataGridTextColumn();
-
-                gridCol.Header = x.ColumnName;
-                gridCol.Binding = new Avalonia.Data.Binding($"Row.ItemArray[{x.Ordinal}]");
-                InputDataTable.Columns.Add(gridCol);
-            }
-        }
-    }
+  }
 }
